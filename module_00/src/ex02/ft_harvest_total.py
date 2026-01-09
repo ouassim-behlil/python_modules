61d9a443d19b2d0bd12ea3a767b3f0a69a95db73
ouassim-behlil <bahlil2001@gmail.com>
2026-01-09 14:33:28 +0100

    use while loop instead of hard coding

--- module_00/src/ex02/ft_harvest_total.py
+++ module_00/src/ex02/ft_harvest_total.py
@@ -1,9 +1,7 @@
 def ft_harvest_total():
     total = 0
     i = 1
-    total += int(input(f"Day {i} harvest: "))
-    i += 1
-    total += int(input(f"Day {i} harvest: "))
-    i += 1
-    total += int(input(f"Day {i} harvest: "))
+    while i < 4:
+        total += int(input(f"Day {i} harvest: "))
+        i += 1
     print("Total harvest:", total)
